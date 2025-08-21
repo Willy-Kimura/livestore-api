06595d6221b03903492a40947b4f14b69d1a5576
Willy Kimura <wilskym@live.com>
2025-08-21 17:58:22 +0300

    Added product favorite field.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,6 +10,6 @@ class Product extends Model
     [
         'brand_id', 'category_id', 'sku', 'name', 'short_desc', 'long_desc', 'regular_price',
         'sale_price', 'status', 'attribute', 'unit', 'rating', 'allow_reviews', 'images',
-        'tags', 'stock', 'low_stock', 'is_free'
+        'tags', 'stock', 'low_stock', 'is_free', 'favorite'
     ];
 }
