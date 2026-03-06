073cf99d21b1f4b9d7229575290bc69059070531
Willy Kimura <wilskym@live.com>
2026-03-06 15:12:48 +0300

    Added cors endpoints.

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,25 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://arumik.co.ke',
+        'https://meneja.arumik.co.ke',
+        'https://admin.tshanic.co.ke',
+        'https://tshanic.co.ke',
+        'http://localhost:5173',
+        'http://192.168.0.29:5173',
+        'http://10.203.109.122:5173',
+        'http://10.111.78.122:5173',
+        'http://10.118.77.122:5173',
+        'http://10.175.102.122:5173',
+        'http://10.249.105.122:5173',
+        'http://10.25.105.122:5173',
+        'http://10.60.70.122:5173',
+        'http://10.108.71.122:5173',
+        'http://10.108.71.122:5174',
+        'http://10.119.209.122:5173',
+        'http://10.180.168.122:5173'
+    ],
 
     'allowed_origins_patterns' => [],
 
