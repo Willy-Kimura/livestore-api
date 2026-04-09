8fa14ff4a497bd9288168711199122fbde9d739f
Willy Kimura <wilskym@live.com>
2026-04-09 16:07:58 +0300

    Cors: Added new local endpoint.

--- config/cors.php
+++ config/cors.php
@@ -25,6 +25,7 @@ return [
         'http://localhost:5173',
         'http://localhost:3000',
         'http://192.168.0.29:5173',
+        'http://192.168.0.34:5173',
         'https://meneja.arumik.co.ke',
         'https://admin.tshanic.co.ke',
         'http://10.203.109.122:5173',
